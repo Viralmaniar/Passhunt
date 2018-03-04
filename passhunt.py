3b6f7b794d64140020439fd6fdf2eb488c812e59
Martin-Zack Mekkaoui <martin@mekkaoui.fr>
2018-03-04 21:58:54 +0100

    Cleanup code, pep8 et al.

--- passhunt.py
+++ passhunt.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python
 '''
-This tool allows you to search for default credentials for routers, network devices, web applications and more. 
-Author: Viral Maniar 
+This tool allows you to search for default credentials for routers,
+network devices, web applications and more.
+Author: Viral Maniar
 Twitter: https://twitter.com/maniarviral
 Github: https://github.com/Viralmaniar
 LinkedIn: https://au.linkedin.com/in/viralmaniar
 '''
-import os, sys
+import sys
 import urllib.request
-import io
 import bs4 as bs
-	
+
+
 def logo():
-	logo = '''
-  ____               _   _             _   
- |  _ \ __ _ ___ ___| | | |_   _ _ __ | |_ 
+    logo = '''
+  ____               _   _             _
+ |  _ \ __ _ ___ ___| | | |_   _ _ __ | |_
  | |_) / _` / __/ __| |_| | | | | '_ \| __|
- |  __/ (_| \__ \__ \  _  | |_| | | | | |_ 
+ |  __/ (_| \__ \__ \  _  | |_| | | | | |_
  |_|   \__,_|___/___/_| |_|\__,_|_| |_|\__|
-	 ***523 vendors, 2084 passwords***
- ░░░░  ███████ ]▄▄▄▄▄▄▄▄          
-█▄█████████████▄█          	      Author: Viral Maniar
-[████████████████████].   	      Twitter: @ManiarViral
- ..◥ ▲ ▲ ▲ ▲ ▲ ▲ ◤..      	      Description: This tool is to search default credentials for routers, network devices, web applications and more. 
-								  
+     ***523 vendors, 2084 passwords***
+ ░░░░  ███████ ]▄▄▄▄▄▄▄▄
+█▄█████████████▄█                    Author: Viral Maniar
+[████████████████████].             Twitter: @ManiarViral
+ ..◥ ▲ ▲ ▲ ▲ ▲ ▲ ◤..                Description: This tool is to search default credentials for routers, network devices, web applications and more.
+
  '''
-	return logo
+    return logo
+
 
 OPTIONS = '''
 1. List supported vendors
@@ -33,26 +35,29 @@ OPTIONS = '''
 3. Exit
 '''
 
+
 def menu():
-	while True:
-		try:
-			choice = str(input('\n[?] Do you want to continue? \n> ')).lower()
-			if choice[0] == 'y':
-				return
-			if choice[0] == 'n':
-				sys.exit(0)
-				break
-		except ValueError:
-			sys.exit(0)
+    while True:
+        try:
+            choice = str(input('\n[?] Do you want to continue? \n> ')).lower()
+            if choice[0] == 'y':
+                return
+            if choice[0] == 'n':
+                sys.exit(0)
+                break
+        except ValueError:
+            sys.exit(0)
+
 
 def checkInternetConnection():
-		try:
-			urllib.request.urlopen('https://cirt.net/')
-		except:
-			print('[!] No internet connection...Please connect to the Internet')
-		else:
-			print('[+] Checking Internet connection...')
-						
+        try:
+            urllib.request.urlopen('https://cirt.net/')
+        except:
+            print('[!] No internet connection...Please connect to the Internet')
+        else:
+            print('[+] Checking Internet connection...')
+
+
 def formatTable(table):
     text = ''
     rows = table.find_all('tr')
@@ -64,51 +69,50 @@ def formatTable(table):
 
     return text
 
+
 def cmd_vendorSearch():
+    vendor = input('Enter Vendor Name:').lower()
+    urlenc = urllib.parse.quote(vendor)
+    url = "https://cirt.net/passwords?vendor=" + urlenc
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    soup = bs.BeautifulSoup(response, "html.parser")
+    for links in soup.find_all('table'):
+        print(links.text)
+        print(formatTable(links))
 
-	vendor = input('Enter Vendor Name:').lower()
-	urlenc = urllib.parse.quote(vendor)
-	url = "https://cirt.net/passwords?vendor=" + urlenc
-	request = urllib.request.Request(url)
-	response = urllib.request.urlopen(request)
-	#response = urllib.quote(request)
-	#print (response.read().decode('utf-8'))
-	soup = bs.BeautifulSoup(response, "html.parser")
-	#print(soup.find_all('a'))
-	for links in soup.find_all('table'):
-		print(links.text)
-		print(formatTable(links))
-		
 
-def cmd_openFile():	
-	path = './vendors.txt'
-	vendors_file = open(path,'r')
-	vendors = vendors_file.read()
-	print(vendors)
-	
+def cmd_openFile():
+    path = './vendors.txt'
+    vendors_file = open(path, 'r')
+    vendors = vendors_file.read()
+    print(vendors)
+
+
 cmds = {
-	"1" : cmd_openFile,
-	"2"	: cmd_vendorSearch,
-	"3"	: lambda: sys.exit(0)
+    "1": cmd_openFile,
+    "2": cmd_vendorSearch,
+    "3": lambda: sys.exit(0)
 }
 
-		
+
 def main():
-	print (logo())
-	checkInternetConnection()
-	try:
-		while True:
-			choice = input("\n%s" % OPTIONS)
-			if choice not in cmds:
-				print ('[!] Invalid Choice')
-				continue
-			cmds.get(choice)()
-	except KeyboardInterrupt:
-		print ('[!] Ctrl + C detected\n[!] Exiting')
-		sys.exit(0)
-	except EOFError:
-		print ('[!] Ctrl + D detected\n[!] Exiting')
-		sys.exit(0)
+    print (logo())
+    checkInternetConnection()
+    try:
+        while True:
+            choice = input("\n%s" % OPTIONS)
+            if choice not in cmds:
+                print ('[!] Invalid Choice')
+                continue
+            cmds.get(choice)()
+    except KeyboardInterrupt:
+        print ('[!] Ctrl + C detected\n[!] Exiting')
+        sys.exit(0)
+    except EOFError:
+        print ('[!] Ctrl + D detected\n[!] Exiting')
+        sys.exit(0)
+
 
 if __name__ == "__main__":
-	main()
+    main()
